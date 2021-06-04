e029eaa52e81fcf15c3b5e9ca441bf20c45ea7ec
mcochi <coro.dri@hotmail.com>
2021-06-04 17:50:17 +0200

    Adecuación a mis repositorios

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/mcochi/themepark_api_demo.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
